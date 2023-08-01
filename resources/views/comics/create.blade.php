db63b3d4df7f0c2c04fa05c923114421738d647f
SimoneVilardo <simovila95@gmail.com>
2023-08-01 17:23:35 +0200

    ultime modifiche

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -22,47 +22,47 @@
                         <div class="row">
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Titolo</label>
-                                <input type="text" name="title" id="title" placeholder="Inserisci il titolo" class="form-control" value="{{ $comic->title }}">
+                                <input type="text" name="title" id="title" placeholder="Inserisci il titolo" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Descrizione</label>
-                                <textarea name="description" id="description" placeholder="Inserisci la descrizione" cols="30" rows="10" class="form-control">{{ $comic->description }}</textarea>
+                                <textarea name="description" id="description" placeholder="Inserisci la descrizione" cols="30" rows="10" class="form-control"></textarea>
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Immagine</label>
-                                <input type="url" name="thumb" id="thumb" placeholder="Inserisci la thumb" class="form-control" value="{{ $comic->thumb }}">
+                                <input type="url" name="thumb" id="thumb" placeholder="Inserisci la thumb" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Cover Immagine</label>
-                                <input type="text" name="cover_image" id="cover_image" placeholder="Inserisci la cover image" class="form-control" value="{{ $comic->cover_image }}">
+                                <input type="text" name="cover_image" id="cover_image" placeholder="Inserisci la cover image" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Immagine 2</label>
-                                <input type="url" name="thumb2" id="thumb2" placeholder="Inserisci la thumb 2" class="form-control" value="{{ $comic->thumb2 }}">
+                                <input type="url" name="thumb2" id="thumb2" placeholder="Inserisci la thumb 2" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Prezzo</label>
-                                <input type="text" name="price" id="price" placeholder="Inserisci il prezzo" class="form-control" value="{{ $comic->price }}">
+                                <input type="text" name="price" id="price" placeholder="Inserisci il prezzo" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Serie</label>
-                                <input type="text" name="series" id="series" placeholder="Inserisci la serie" class="form-control" value="{{ $comic->series }}">
+                                <input type="text" name="series" id="series" placeholder="Inserisci la serie" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Data di Rilascio</label>
-                                <input type="date" name="sale_date" id="sale_date" placeholder="Inserisci la data di rilascio" class="form-control" value="{{ $comic->sale_date }}">
+                                <input type="date" name="sale_date" id="sale_date" placeholder="Inserisci la data di rilascio" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Tipo</label>
-                                <input type="text" name="type" id="type" placeholder="Inserisci il tipo" class="form-control" value="{{ $comic->type }}">
+                                <input type="text" name="type" id="type" placeholder="Inserisci il tipo" class="form-control">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Artisti</label>
-                                <textarea name="artists" id="artists" placeholder="Inserisci gli artisti" cols="30" rows="3" class="form-control">{{ $comic->artists }}</textarea>
+                                <textarea name="artists" id="artists" placeholder="Inserisci gli artisti" cols="30" rows="3" class="form-control"></textarea>
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Scrittori</label>
-                                <textarea name="writers" id="writers" placeholder="Inserisci gli scrittori" cols="30" rows="3" class="form-control">{{ $comic->writers }}</textarea>
+                                <textarea name="writers" id="writers" placeholder="Inserisci gli scrittori" cols="30" rows="3" class="form-control"></textarea>
                             </div>
                             <div class="col-12 text-center my-5">
                                 <button type="submit" class="btn btn-primary">Aggiungi</button>
