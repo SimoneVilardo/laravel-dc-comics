f3c41d183b1cce4c6a4ba9db49f1ed1b51d6f719
SimoneVilardo <simovila95@gmail.com>
2023-08-02 09:48:30 +0200

    ultime modifiche funzione update

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -92,7 +92,19 @@ class ComicsController extends Controller
     {
         $form_data = $this->validation($request->all());
 
-        $comic->update($form_data);
+        $comic->title = $form_data['title'];
+        $comic->description = $form_data['description'];
+        $comic->thumb = $form_data['thumb'];
+        $comic->cover_image = $form_data['cover_image'];
+        $comic->thumb2 = $form_data['thumb2'];
+        $comic->price = $form_data['price'];
+        $comic->series = $form_data['series'];
+        $comic->sale_date = $form_data['sale_date'];
+        $comic->type = $form_data['type'];
+        $comic->artists = $form_data['artists'];
+        $comic->writers = $form_data['writers'];
+
+        $comic->save();
 
         return redirect()->route('comics.show', compact('comic'));
     }
